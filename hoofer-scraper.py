a2e4a0a4a175e034144c9538804593e913dba89e
Jed Pulley <jedediah.pulley@gmail.com>
2024-07-21 18:45:56 -0500

    allow user input for date ranges

--- hoofer-scraper.py
+++ hoofer-scraper.py
@@ -1,33 +1,40 @@
-# import sys
-# sys.path.insert(0,'C:\\Users\\Jeded\\AppData\\Local\\Programs\\Python\\Python311\\Lib\\site-packages')
-
+import sys
 import csv
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
-# Base URL
-base_url = "https://lessons.hoofersailing.org/event/"
-
+if len(sys.argv) < 3:
+    print("Enter both start and end ranges")
+    sys.exit()
 
 # Range of event numbers to loop through
-start_event_number = 108300
-end_event_number = 108450
+start_range = sys.argv[1]
+end_range = sys.argv[2]
 
-# CSV file name
-csv_file = "hoofer_lessons_data.csv"
+# exit program if inputs invalid
+if not start_range.isnumeric() or not end_range.isnumeric():
+    print("Invalid inputs: ensure start and end ranges are numeric")
+    sys.exit()
+
+start_range = int(start_range)
+end_range = int(end_range)
+
+csv_file = datetime.today().strftime('%m-%d_%H%M') + '-hoofer-lessons.csv'
 
 # Open the CSV file for writing
 with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     
     # Write the header row
-    writer.writerow(["Title", "Schedule", "URL"])
+    writer.writerow(["Title", "Schedule", "Event URL", "Registration URL"])
     
     # Loop through the event numbers
-    for event_number in range(start_event_number, end_event_number + 1):
+    for event_number in range(start_range, end_range + 1):
         # Construct the full URL
-        url = f"{base_url}{event_number}"
+        url = f"https://lessons.hoofersailing.org/event/{event_number}"
         trimmed_url = f"lessons.hoofersailing.org/event/{event_number}"
+        register_url = f"lessons.hoofersailing.org/event/register/{event_number}"
         
         try:
             # Fetch the HTML content
@@ -42,15 +49,7 @@ with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
             time = soup.find(class_="event-schedule").get_text(strip=True)
                         
             # Write the data to the CSV file
-            writer.writerow([title, time, trimmed_url])
-            
-            # Print the results to the console
-            # print(f"Title: {title}")
-            # print(f"Time: {time}")
-            # print(f"URL: {url}")
-            # print("-" * 50)
+            writer.writerow([title, time, trimmed_url, register_url])
         
         except requests.RequestException as e:
             print(f"Failed to fetch {url}: {e}")
-
-# End of script
